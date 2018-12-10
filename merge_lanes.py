158359998333d37e6e4eddda160b82831a31cb5b
ggabernet <gisela.gabernet@qbic.uni-tuebingen.de>
2018-12-10 15:40:38 +0100

    finished script

--- merge_lanes.py
+++ merge_lanes.py
@@ -1,9 +1,24 @@
-import os
-wd = os.getcwd()
+import subprocess
+import argparse
 
-fname = "QMHBS-Hasselman_postman.txt"
 
-while open(fname, mode = 'r') as f:
-    codes = file.readlines()
+parser = argparse.ArgumentParser()
+parser.add_argument("file", type=str, help="File with QBiC codes.")
+parser.add_argument("-pR1", "--patternR1", type=str, help="Pattern to find R1 of paired-seq.", default='_R1_')
+parser.add_argument("-pR2", "--patternR2", type=str, help="Pattern to find R2 of paired-seq.", default='_R2_')
 
-print codes
+args = parser.parse_args()
+
+with open(args.file, mode='r') as f:
+    lines = f.readlines()
+
+codes = [line.rstrip() for line in lines]
+
+for sample in codes:
+    subprocess.call("mkdir %s" % sample, shell=True, stdout=True)
+    subprocess.Popen("mv %s* %s" % (sample, sample), shell=True, stdout=True)
+    subprocess.Popen("find %s -name '*%s*.fastq.gz' -exec cat {} ';' > %s/%s_R1_.fastq.gz" % (sample, args.patternR1, sample, sample), shell=True, stdout=True)
+    subprocess.Popen("find %s -name '*%s*.fastq.gz' -exec cat {} ';' > %s/%s_R2_.fastq.gz" % (sample, args.patternR2, sample, sample), shell=True, stdout=True)
+
+
+
