5129d4c2f1ed00a4f4bd26a8ba128657aa62c691
ggabernet <gisela.gabernet@qbic.uni-tuebingen.de>
2021-02-14 12:24:21 +0100

    add description remove metadata

--- merge_lanes/merge_lanes.sh
+++ merge_lanes/merge_lanes.sh
@@ -16,7 +16,11 @@
 
 # mail alert at start, end and abortion of execution
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=gisela.gabernet@gmail.com
+#SBATCH --mail-user=<user.email@mail.com>
+
+# Takes txt file with newline separated codes. Finds fastq.gz files starting with the code,
+# and merges the fastq files with the same code from different lanes, naming the files as "code_{R1,R2}.fastq.gz".
+# creates a folder named as "code", and moves the files corresponding to that code there.
 file="codes.tsv"
 while IFS= read -r f1
 do
@@ -24,7 +28,6 @@ do
 	mkdir $f1
     printf 'Making folder %s \n' "$f1"
 	mv $f1*.fastq.gz $f1/
-	mv $f1*.metadata $f1/
 	find $f1 -type f -name '*_R1_*.fastq.gz' -print0 | sort -z | xargs -0 cat > "$f1"/"$f1"_R1.fastq.gz
     find $f1 -type f -name '*_R2_*.fastq.gz' -print0 | sort -z | xargs -0 cat > "$f1"/"$f1"_R2.fastq.gz
 done <"$file"
